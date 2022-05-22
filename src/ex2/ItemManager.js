5ba43cdff4b1cbc90e12224fb980c08440666074
Ori Marom <ori.marom10@gmail.com>
2022-05-22 10:22:39 +0300

    ItemManager init

--- src/ex2/ItemManager.js
+++ src/ex2/ItemManager.js
@@ -1,1 +1,22 @@
+class ItemManager {
+ 
+   constructor(list){
+    
+     this.todoList = list
+     this.todos = []
+  
+   }
 
+  start() {
+    console.log("you are in start method");
+  }
+
+   addTodo(event) {
+  console.log("hi")
+  console.log(this.todos)
+
+    }
+   
+    
+
+}
--- src/ex2/main.js
+++ src/ex2/main.js
@@ -1,34 +1,26 @@
+
+
 // Implement the `Main` class here
 class Main {
   
+ 
 
   init() {
-    //Selectors
-    const todoInput = document.getElementById("input-txt");
+
+   
+    const itemManager = new ItemManager()
+    console.log(itemManager.inputValue)
+
+    const todoInput = document.getElementById("input-txt"); 
     const todoButton = document.getElementById("add-btn");
-    const todoList = document.getElementById("list-element");
-    const clearAllBtn = document.getElementById("clearAll-btn");
-    const sortBtn = document.getElementById("sort-btn");
-    const inputBox = document.querySelector(".inputField input");
-    const svgContainer = document.getElementById("svg");
-    const todoElement = document.getElementById("list-element");
-    const footerSpan = document.querySelector(" .footer span ");
+    todoButton.addEventListener("click", itemManager.addTodo);
+  
 
-    const animItem = bodymovin.loadAnimation({
-        wrapper: svgContainer,
-        animType: "svg",
-        loop: false,
-        autoplay: false,
-        path: "https://assets4.lottiefiles.com/private_files/lf30_5aubt2fy.json",
-      });
-      
-      const pendingTasksCount = document.querySelector(".pendingTasksCount");
-      const ENTER_KEY = 13;
-      const TRASH_ANIMATION_TIMEOUT = 500;
-
-      
   }
+
 }
+
+
 
 const main = new Main();
 
@@ -37,3 +29,8 @@ document.addEventListener("DOMContentLoaded", function () {
   // the method should add the event listener to your "add" button
   main.init();
 });
+
+
+
+
+
