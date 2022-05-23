64842d216cad8be78567097a0291050e087ffcc7
Ori Marom <ori.marom10@gmail.com>
2022-05-24 00:40:11 +0300

    add Item as an object functionality

--- src/ex2/ItemManager.js
+++ src/ex2/ItemManager.js
@@ -1,27 +1,31 @@
 import PokemonClient from "./PokemonClient.js";
 import UIManager from "./UIManager.js";
+import Item from "./ItemClass.js";
 
 class ItemManager {
   constructor() {
     this.pokemonClient = new PokemonClient();
-    this.itemList = [];
+    this.itemList = []
   }
 
-  handleAddTodo(textItem){
-      this.addTodo(textItem)
-      //save to local storage
+  handleAddTodo(itemTextValue){
+      this.addTodo(itemTextValue)
+      //need to add also save to local storage
       if (this.itemList.length == 1){
       UIManager.showButtonsAndFooter()
-      UIManager.UIHandleAddItem(textItem, this.itemList.length)
+      UIManager.UIHandleAddItem(itemTextValue, this.itemList.length)
       return
       }
-      UIManager.UIHandleAddItem(textItem, this.itemList.length)
+      UIManager.UIHandleAddItem(itemTextValue, this.itemList.length)
 
   }
   
   
-  addTodo(textItem) {
-    this.itemList.push(textItem);
+  addTodo(itemTextValue) {
+    //Item objects are pushed to the list (not just strings)
+    //Extendable in case we would like to save more data in the future
+    this.item = new Item(itemTextValue)
+    this.itemList.push(this.item);
   }
 
   async fetchPokemonAndAddToList(pokemonNumber) {
