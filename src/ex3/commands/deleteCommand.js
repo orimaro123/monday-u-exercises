35b728fe25f8b3980ce3f381e06c21d6b5a9651a
Ori Marom <ori.marom10@gmail.com>
2022-06-09 18:28:32 +0300

    fix delete task bug

--- src/ex3/commands/deleteCommand.js
+++ src/ex3/commands/deleteCommand.js
@@ -11,9 +11,14 @@ export async function deleteTask(inputValue) {
   if(!itemList.length){
     return console.log(chalk.bgRedBright("The list is empty"))
   }
+  if(inputValue - 1 >= 0 && inputValue < itemList.length){
   const itemToRemove = itemList[inputValue -1].name
   itemManager.removeFromItemListByIndex(inputValue)
   console.log(chalk.bgYellowBright(`${itemToRemove} was removed successfully`))
+  }
+  else {
+    console.log(chalk.bgYellowBright(`task number ${inputValue} does not exist`))
+  }
 
  
 }
