7ec6112e88e3bff787704060224050d0385d850e
Ori Marom <ori.marom10@gmail.com>
2022-07-03 11:49:37 +0300

    improve styling

--- src/client/src/components/AppContainer.jsx
+++ src/client/src/components/AppContainer.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback, useMemo } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import ListControls from "./ListControls";
 import List from "./List";
 import { Button, Toast } from "monday-ui-react-core";
