d4035aebbb6e5dd7b7717bcfb5e656864bdcad10
Ori Marom <ori.marom10@gmail.com>
2022-07-07 14:43:29 +0300

    move getQueryName to selectors file

--- src/client/src/components/Search.jsx
+++ src/client/src/components/Search.jsx
@@ -5,24 +5,26 @@ import { Button } from "monday-ui-react-core";
 import PropTypes from "prop-types";
 import { useDispatch, useSelector } from "react-redux";
 import { updateQueryName } from "../redux/actions/itemsEntitiesActions";
-
-// todo: move this to selectors
-const getQueryName = (state) => state?.itemsEntities?.query?.name ?? '';
+import { getQueryName } from "../redux/selectors/itemsEntitiesSelectors";
 
 function Search({}) {
-  const name = useSelector(getQueryName)
-  const dispatch = useDispatch()
+  const name = useSelector(getQueryName);
+  const dispatch = useDispatch();
 
   const handleChange = (e) => {
     e.preventDefault();
     dispatch(updateQueryName(e.target.value));
-  }
+  };
 
   return (
     <div className="search-div">
-      <input type="text" id="search-input" placeholder="Search... " value={name} onChange={handleChange} />
-
-   
+      <input
+        type="text"
+        id="search-input"
+        placeholder="Search... "
+        value={name}
+        onChange={handleChange}
+      />
     </div>
   );
 }
--- src/client/src/redux/selectors/itemsEntitiesSelectors.js
+++ src/client/src/redux/selectors/itemsEntitiesSelectors.js
@@ -4,3 +4,4 @@ export const getItemsCount = (state) => getItemsEntities(state).itemsCount;
 export const getItems = (state) => getItemsEntities(state).items;
 
 export const getQuery = (state) => state.itemsEntities.query;
+export const getQueryName = (state) => state?.itemsEntities?.query?.name ?? ''
